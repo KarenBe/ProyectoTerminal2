fc12c91aede5cfc6d2d7e74e75f6d3cf97290121
karenBe <kcortezd1400@alumno.ipn.mx>
2019-04-29 02:39:18 -0500

    leer celdas de la matriz

--- prueba.py
+++ prueba.py
@@ -1,11 +1,21 @@
 import cv2
 from coloresReferencia import coloresReferencia 
+from pruebaCeldas import muestraDeColor
 
-image = cv2.imread('img14.png')
-imagen = coloresReferencia(image,14,2)
+tamCelda = 20
+tamMatriz = 12
+numColores = 4
+
+image = cv2.imread('img12.png')
+imagen = coloresReferencia(image,tamMatriz,numColores)
+
 coloresR = imagen.obtenerColoresReferencia()
-print("colores referencia: "coloresR)
-
+print("colores referencia: ",coloresR)
 cv2.imshow('original', image)
+
+matriz = muestraDeColor(image,tamMatriz,coloresR,numColores)
+matriz.celda()
+
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
